dc76566c82c36dbbd2c9c5e026a475dd18cca997
sayuveos11 <kkallinberg@gmail.com>
2026-09-11 19:29:32 +0300

    feat: add automatic task IDs and task planner menu

---
+++ main.py
@@ -0,0 +1,15 @@
+from task import Task
+from task_manager import TaskManager
+
+user_input = int(input("""
+===== TASK PLANNER =====
+
+1. Add task
+2. Show all tasks
+3. Show completed tasks
+4. Show active tasks
+5. Complete task
+6. Update task
+7. Delete task
+0. Exit
+"""))
